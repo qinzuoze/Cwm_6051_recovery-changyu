69511570fcf7a70cfe98c604f1484ab450ee6f24
changyuet <10180016@qq.com>
2015-03-09 14:35:27 +0800

    First Update Cwm Recovery

---
+++ default_recovery_ui.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <linux/input.h>
+
+#include "common.h"
+#include "extendedcommands.h"
+#include "recovery_ui.h"
+
+char* MENU_HEADERS[] = { NULL };
+#ifndef USE_CHINESE_FONT
+char* MENU_ITEMS[] = { "reboot system now",
+                       "install zip",
+                       "wipe data/factory reset",
+                       "wipe cache partition",
+                       "backup and restore",
+                       "mounts and storage",
+                       "advanced",
+                       NULL };
+#else
+char* MENU_ITEMS[] = { "重启设备",
+                       "安装ZIP刷机包",
+                       "清除数据恢复出厂",
+                       "清除Cache分区",
+                       "备份和还原备份",
+                       "挂载和大容量储存",
+                       "高级选项",
+                       NULL };
+#endif
+void device_ui_init(UIParameters* ui_parameters) {
+}
+
+int device_recovery_start() {
+    return 0;
+}
+
+// add here any key combo check to reboot device
+int device_reboot_now(volatile char* key_pressed, int key_code) {
+    return 0;
+}
+
+int device_perform_action(int which) {
+    return which;
+}
+
+int device_wipe_data() {
+    return 0;
+}
